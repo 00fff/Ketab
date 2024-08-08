8cdbffc66deb09783ea50bf71cd214b8eb736bc3
tanite <harithsullaiman@gmail.com>
2024-08-07 21:05:25 -0400

    Added BareBones React Front End

---
+++ client/app/(tabs)/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView, View, Text} from 'react-native';
+
+const home = () => {
+  return (
+    <ScrollView>
+    <View>
+      <Text>home</Text>
+    </View>
+    </ScrollView>
+  );
+};
+export default home;
---
+++ client/app/(tabs)/list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const list = () => {
+  return (
+    <View>
+      <Text>list</Text>
+    </View>
+  );
+};
+export default list;
