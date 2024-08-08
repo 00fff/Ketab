48094fdb62f24edf5c0d026538fa76b69f3fb6ba
tanite <harithsullaiman@gmail.com>
2024-08-08 19:37:05 -0400

    Set Up Sign Up And Login Screen

--- client/app/LogIn.jsx
+++ client/app/LogIn.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { Redirect, useRouter } from "expo-router";
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
-
-const Signup = () => {
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+const LogIn = () => {
   const router = new useRouter;
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => {
+    setShowPassword(!showPassword)
+    console.log(showPassword)
+  }
   return (
     <SafeAreaView style={{ backgroundColor: "#0E3B43", flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <View>
-        <Text style={{ fontWeight: '900', fontSize: 40, color: '#FFF' }}>Sign Up</Text>
+        <Text style={{ fontWeight: '900', fontSize: 40, color: '#FFF' }}>Log In</Text>
       </View>
       <View style={{ padding: 20, width: '80%' }}>
         <Text style={{ fontWeight: '900', fontSize: 15, color: '#FFF' }}>Email:</Text>
@@ -31,22 +35,6 @@ const Signup = () => {
           placeholderTextColor="#888"
           keyboardType="email-address"
         />
-        <Text style={{ fontWeight: '900', fontSize: 15, color: '#FFF', marginTop: 20 }}>Username:</Text>
-        <TextInput
-          style={{
-            borderWidth: 1,
-            borderColor: '#357266',
-            borderRadius: 8,
-            padding: 10,
-            marginVertical: 10,
-            color: '#FFF',
-            backgroundColor: '#1A4D56'
-          }}
-          value={name}
-          onChangeText={setName}
-          placeholder="John Doe"
-          placeholderTextColor="#888"
-        />
         <Text style={{ fontWeight: '900', fontSize: 15, color: '#FFF', marginTop: 20 }}>Password:</Text>
         <TextInput
           style={{
@@ -58,12 +46,22 @@ const Signup = () => {
             color: '#FFF',
             backgroundColor: '#1A4D56'
           }}
+          
           value={password}
+          secureTextEntry={showPassword ? false : true}
           onChangeText={setPassword}
           placeholder="Password"
           placeholderTextColor="#888"
+          
         />
-        <TouchableOpacity
+            <TouchableOpacity onPress={togglePassword}>
+            <MaterialCommunityIcons
+              name={showPassword ? "eye" : "eye-off"}
+              size={24}
+              color="#FFF"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
           style={{
             backgroundColor: '#357266',
             borderRadius: 8,
@@ -73,15 +71,12 @@ const Signup = () => {
           }}
           onPress={() => console.log('Sign Up button pressed')}
         >
+         
           <Text style={{ color: '#FFF', fontWeight: '900' }}>Sign Up</Text>
-        </TouchableOpacity>
-        <Text />
-        <TouchableOpacity onPress={() => router.push('/LogIn')}>
-          <Text>                         Already Have An Account?</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
-export default Signup;
+export default LogIn;
