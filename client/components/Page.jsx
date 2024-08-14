434887e9b0f63db21ee8ed69d153072929837b2b
tanite <harithsullaiman@gmail.com>
2024-08-14 17:02:47 -0400

    Added Page JSX

---
+++ client/components/Page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView, SafeAreaView, View, TextInput, Text, TouchableOpacity} from 'react-native';
+import { CameraView, CameraType, useCameraPermissions} from 'expo-camera';
+import { useRoute } from '@react-navigation/native'; // Import useRoute
+const Page = () => {
+  return (
+    <SafeAreaView>
+    <ScrollView>
+    <View>
+        <Image source=''/>
+    </View>
+    </ScrollView>
+    </SafeAreaView>
+    
+  );
+  
+};
+
+export default Page;
