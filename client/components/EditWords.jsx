85cda374f9a07b9e862168a003569ed202e209db
tanite <harithsullaiman@gmail.com>
2024-08-22 17:05:33 -0400

    Text Form Now Has Multiple Lines and cane be toggled from Form

--- client/components/EditWords.jsx
+++ client/components/EditWords.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, Button, Image, StyleSheet, Touchable, TouchableOpacity, TextInput } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Ionicons } from 'react-native-vector-icons';
 import axios from 'axios'; // Import axios for making HTTP requests
 
-const EditWords = ({ width, height, left, right, color, bottom, id, currentText}) => {
+const EditWords = ({ width, height, left, right, color, bottom, id, currentText, changeEditState}) => {
   const [text, onChangeText] = React.useState(currentText);
+  const [editWords, seteditWords] = useState(false)
+
 
   return (
     <SafeAreaView style={[styles.container, { width, height, left, right, backgroundColor: color, bottom }]}>
+      <TouchableOpacity onPress={() => changeEditState()} style={{position: 'absolute', top: 0, right: 0, padding: 10, }}><Ionicons name="pencil-outline" color={"black"} size={30} /></TouchableOpacity>
       <View style={styles.innerContainer}>
-        <TextInput  style={{height: 100, width: 100}} onChangeText={onChangeText} value={text}/>
-          
       
+        <TextInput  style={{height: 350, width: 200}} onChangeText={onChangeText}  value={text}
+          multiline={true} // Allows multiline input
+          textAlignVertical="top" // Aligns text to the top
+          autoCapitalize="none" // Prevents automatic capitalization
+          autoCorrect={false} />
+          <TouchableOpacity style={{ margin: 10, height: 40, width: 200, backgroundColor: '#0e3b43', justifyContent: 'center', alignItems: 'center', borderRadius: 10, }}>Submit</TouchableOpacity>
       </View>
     </SafeAreaView>
   );
