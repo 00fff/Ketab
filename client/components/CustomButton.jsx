f685290fa9fd5b45f66d27103b07957c077830d4
tanite <harithsullaiman@gmail.com>
2024-09-09 19:21:24 -0400

    Fixed Sign Up Button

--- client/components/CustomButton.jsx
+++ client/components/CustomButton.jsx
@@ -4,18 +4,18 @@ import { TouchableOpacity, View, Text } from 'react-native';
 const CustomButton = ({ onPress, color, text }) => {
   return (
     <View style={{
-      borderColor: 'black', // Apply border color to the View
-      borderWidth: 1, // Make sure the border width is set to a numeric value
       borderRadius: 8,
       marginTop: 30,
     }}>
       <TouchableOpacity 
         onPress={onPress} 
         style={{
-          backgroundColor: color, // Use color prop for background color
-          borderRadius: 4,
+          backgroundColor: '#357266',
+          borderRadius: 8,
           paddingVertical: 15,
+          
           alignItems: 'center',
+          width: 100,
         }}>
         <Text style={{ color: '#FFF' }}>{text}</Text>
       </TouchableOpacity>
